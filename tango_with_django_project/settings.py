913b8d52eae1d07e7e66ea2448c0af1c424d4730
“najmathummer” <najmathu8@gmail.com>
2022-01-27 21:31:39 +0000

    display "Rango says hey there partner!"

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5zw7-m4j5*+!td4s0ji%$w9q4@g6@s8h9r68k-4%iqe&k!c2&p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*'] 
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rango',
 ]
 
 MIDDLEWARE = [
