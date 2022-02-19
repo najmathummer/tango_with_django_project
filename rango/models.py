f33b7758129a32e81c46e52a59d4472447286dd8
“najmathummer” <najmathu8@gmail.com>
2022-02-19 22:05:25 +0000

    Complete chapter 7.

--- rango/models.py
+++ rango/models.py
@@ -3,9 +3,11 @@ from django.template.defaultfilters import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    NAME_MAX_LENGTH = 128
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
+
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super(Category, self).save(*args, **kwargs)
